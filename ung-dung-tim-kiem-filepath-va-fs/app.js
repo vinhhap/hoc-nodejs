d7519676f334aefb6affe8052383445e05528ee7
Quan Nguyen <nguyennamquan1996@gmail.com>
2016-02-15 10:17:11 +0000

    Lam lai ung dung tim kiem file fs

--- ung-dung-tim-kiem-filepath-va-fs/app.js
+++ ung-dung-tim-kiem-filepath-va-fs/app.js
@@ -1,11 +1,28 @@
-var fs = require('fs'),
-    path = require('path'),
-    name = 'test.js';
+var fs = require('fs');
+var path = require('path');
+var processData = process.argv;
+var result = [];
 
-var timKiem = function(name) {
-    var currentPath = __dirname;
-    fs.stat(path.join(currentPath, name), function(err, stats) {
-      stats == undefined ? console.log("Khong ton tai") : console.log("duong dan den file: " + path.join(currentPath, name));
-    });  
+var check = function(directory, fileName) {
+    var files = fs.readdirSync(directory);
+    files.forEach(function(file) {
+        var stat = fs.statSync(path.join(directory, file));
+        if (stat.isFile()){
+            if (file == fileName){
+                result.push(path.join(directory, fileName));
+            }
+        } else {
+            check(path.join(directory,file),fileName);
+        }
+    });
 };
-timKiem(name);
+
+check(processData[3], processData[2]);
+if(result.length > 0) {
+    console.log("Tim thay " + result.length + " file tai cac thu muc: ");
+    result.forEach(function(direct) {
+       console.log(direct); 
+    });
+} else {
+    console.log("Khong tim thay file");
+}
